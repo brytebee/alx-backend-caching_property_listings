b63a38bae9e3e1db2a7d1ed548aaf8ad19ff322a
Bright Atsighi <brytebee@gmail.com>
2025-08-29 16:08:05 +0100

    Updated property views

--- properties/views.py
+++ properties/views.py
@@ -1,9 +1,15 @@
 # properties/views.py
 from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
 from .models import Property
 
+
+@cache_page(60 * 15)
 def property_list(request):
     properties = Property.objects.all().values(
         "id", "title", "price", "location"
-    )  # pick fields you want
-    return JsonResponse(list(properties), safe=False)
+    )
+
+    return JsonResponse({
+        "data": list(properties)
+    })
